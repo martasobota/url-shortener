f42fe18c69b541a2538f3c3d53382793914517c4
martazaryn <martazaryn@gmail.com>
2017-08-18 16:47:38 +0200

    Deleted redundant import: patterns, not supported since DJango 1.8

--- url_shortener/app_url_shortener/urls.py
+++ url_shortener/app_url_shortener/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import url, patterns, include
+from django.conf.urls import url
  
-urlpatterns = patterns('app_url_shortener.views',
+# urlpatterns = patterns('app_url_shortener.views',
+#     url(r'^$', 'index', name='home'),
+#     url(r'^shorten/$', 'shorten_url', name='shortenurl'),
+#     )
+
+urlpatterns = [
+	# 'app_url_shortener.views',
     url(r'^$', 'index', name='home'),
-    url(r'^(?P&lt;short_id&gt;\w{6})$', 'redirect_to_original', name='redirectoriginal')
     url(r'^shorten/$', 'shorten_url', name='shortenurl'),
-    )
+]
